1429e2d22b68d05439838c5950346537da865fe3
mahi42511 <mahidevops2022@gmail.com>
2023-11-08 12:11:16 +0000

    added new lines

--- HelloWorld.java
+++ HelloWorld.java
@@ -40,5 +40,15 @@ public class HelloWorld {
     System.out.println("Hello, canada");
     System.out.println("Hello, kadapa ");
     System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
+    System.out.println("Hello, wed......................");
   }
 }
